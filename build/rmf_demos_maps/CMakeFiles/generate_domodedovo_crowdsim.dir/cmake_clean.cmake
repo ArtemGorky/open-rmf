275d66a6e12d5fd92a95e87c2727469eeda79a59
root <root@open-rmf.ru-central1.internal>
2024-08-15 13:25:31 +0000

    v15.08.24-16:23

---
+++ build/rmf_demos_maps/CMakeFiles/generate_domodedovo_crowdsim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_domodedovo_crowdsim"
+  "domodedovo_crowdsim"
+  "maps/domodedovo/domodedovo.world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/generate_domodedovo_crowdsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/rmf_demos_maps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/rmf_ws/src/demonstrations/rmf_demos/rmf_demos_maps
+# Build directory: /root/rmf_ws/src/demonstrations/rmf_demos/build/rmf_demos_maps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
