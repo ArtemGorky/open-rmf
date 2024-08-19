eba0c7f1aed435f5e7d09b3976707ab92cd82846
ArtemGorky <70753374+ArtemGorky@users.noreply.github.com>
2024-08-19 16:12:58 +0300

    Delete build directory

--- build/rmf_demos_maps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/rmf_ws/src/demonstrations/rmf_demos/rmf_demos_maps
-# Build directory: /root/rmf_ws/src/demonstrations/rmf_demos/build/rmf_demos_maps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
